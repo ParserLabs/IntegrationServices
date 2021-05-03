7c29ecf46806637dcb305409f9aa38408ff5ab77
deepakp <deepakp@dewsolutions.in>
2021-05-03 23:10:52 +0530

    skelaton creation, first commit

---
+++ src/main/java/com/parserlabs/commons/IntegrationServicesApplication.java
@@ -0,0 +1,13 @@
+package com.parserlabs.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationServicesApplication.class, args);
+	}
+
+}
