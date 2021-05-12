f5b4c6ece35a816765cd1032e34c2e479cfe5cd3
deepakp <deepakp@dewsolutions.in>
2021-05-12 09:57:49 +0530

    HID-333: minor changes

--- src/test/java/com/parserlabs/commons/IntegrationServicesApplicationTests.java
+++ src/test/java/com/parserlabs/commons/IntegrationServicesApplicationTests.java
@@ -1,13 +1,13 @@
-package com.parserlabs.commons;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class IntegrationServicesApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.parserlabs.commons;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+////@SpringBootTest
+//class IntegrationServicesApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
